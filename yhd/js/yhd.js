66a928a783db9afd4d72a5acd9400caf194dab0e
刘慧冬 <1056569214@qq.com>
2020-04-19 18:32:01 +0800

    修改更新头部

--- yhd/js/yhd.js
+++ yhd/js/yhd.js
@@ -132,3 +132,19 @@ opt.onclick = function () {
     opt.style.display = "none";
     ipt.value = "";
 }
+// 登录显示用户名
+var userInfo=document.getElementsByClassName("login-txt")[0].children;
+var userName=sessionStorage.getItem("userName");
+if(userName){
+    userInfo[0].parentElement.previousElementSibling.style.display="none";
+    userInfo[0].parentElement.style.display="block";
+    userInfo[1].innerHTML=userName;
+    userInfo[3].children[0].children[1].innerHTML=userName;
+}else{
+    userInfo[0].parentElement.previousElementSibling.style.display="block";
+    userInfo[0].parentElement.style.display="none";
+}
+function exitUser() {
+    userInfo[0].parentElement.style.display="none";
+    userInfo[0].parentElement.previousElementSibling.style.display="block";
+}
