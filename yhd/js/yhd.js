0010cda6dfbed75ad67d418c19bc680bb1272495
PC-20200325HOAA\Administrator <1643645148@qq.com>
2020-04-20 21:18:14 +0800

    订单预约静态页面

--- yhd/js/yhd.js
+++ yhd/js/yhd.js
@@ -125,3 +125,20 @@ opt.onclick=function() {
     cTxt.innerHTML=opt.innerHTML;
     opt.style.display="none";
 }
+// 登录显示用户名
+var userInfo=document.getElementsByClassName("login-txt")[0].children;
+var userName=sessionStorage.getItem("userName");
+if(userName){
+    userInfo[0].parentElement.previousElementSibling.style.display="none";
+    userInfo[0].parentElement.style.display="block";
+    userInfo[1].innerHTML=userName;
+    userInfo[3].children[0].children[1].innerHTML=userName;
+}else{
+    userInfo[0].parentElement.previousElementSibling.style.display="block";
+    userInfo[0].parentElement.style.display="none";
+}
+function exitUser() {
+    userInfo[0].parentElement.style.display="none";
+    userInfo[0].parentElement.previousElementSibling.style.display="block";
+    sessionStorage.removeItem("userName");
+}
