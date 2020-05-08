e11a86c982f81140940411d4bf8b7bd495388154
poor-or <l>
2020-05-08 12:06:34 +0800

    no message

--- test/zxy/yhd/php/pro-city/city.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    $con=new mysqli("localhost", "root", "123456", "sign");
-    $txt=$con->query("select * from tb_city;");
-    while($list=$txt->fetch_assoc()){
-        $lists[]=$list;      
-    }
-    echo json_encode($lists);
-?>
